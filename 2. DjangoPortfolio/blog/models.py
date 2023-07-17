91c52d133af1ca069a5647f2fa986b0e740fc7ca
LozanoJohan <lozanojohan321@gmail.com>
2023-07-17 17:48:19 -0500

    End portfolio

--- 2. DjangoPortfolio/blog/models.py
+++ 2. DjangoPortfolio/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog/images/')
+    date = models.DateField(datetime.date.today())
